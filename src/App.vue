9cc43671a2b6bf6bfde72781c6192e6bfee44ca3
camilo cuervo <camilo267@hotmail.es>
2021-07-22 20:57:41 +1000

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-main>
+      <v-container class="container">
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+:root {
+  --max-width: 600px;
+  background-color: rgb(222, 210, 226) !important;
 }
 
-#nav {
-  padding: 30px;
+html {
+  font-size: 100%;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+body {
+  font-size: 1rem;
+  margin: 0;
+  font-family: "Poppins", sans-serif;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+p {
+  font-size: 1.25rem;
+  line-height: 2;
+}
+
+.container {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: var(--max-width);
+  padding: 1.5rem;
 }
 </style>
